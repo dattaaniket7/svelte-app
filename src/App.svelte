11863cb6a8636432cfaf361500cdfa040549f360
Aniket Datta <datta.aniket7@gmail.com>
2023-09-21 00:12:24 +0530

    dynamic-attributes

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
-  let name = "oldMonk";
+  let name = "Stuart";
+  import src from "./assets/giphy.gif";
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
+<img {src} alt="{name} alert" />
