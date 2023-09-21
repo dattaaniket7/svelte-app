f0eec4c9aeb0b063d1ffc48072b42289f63e9a51
Aniket Datta <datta.aniket7@gmail.com>
2023-09-21 05:57:49 +0530

    reactivity-assignments

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,22 @@
   let name = "Stuart <button>Minnion</button>";
   import src from "./assets/giphy.gif";
   import Nested from "./lib/Nested.svelte";
+  let counter = 0;
+
+  function increment() {
+    counter += 1;
+  }
 </script>
 
-<h1>Hello {@html name.toUpperCase()}</h1>
+<!-- <h1>Hello {@html name.toUpperCase()}</h1>
 <img {src} alt="{name} alert" />
 <Nested />
+<button on:click={increment}>
+  Increase my counter {counter}
+</button> -->
+<h1>{counter}</h1>
+<button on:click={() => (counter += 1)}>Increment</button>
+<button on:click={() => (counter -= 1)}>Decrement</button>
 
 <style>
   h1 {
