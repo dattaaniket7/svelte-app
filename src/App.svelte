43c5721c0532cbb3bd185b505a798b9e81fc497e
Aniket Datta <datta.aniket7@gmail.com>
2023-09-21 05:46:06 +0530

    html-tags

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-  let name = "Stuart";
+  let name = "Stuart <button>Minnion</button>";
   import src from "./assets/giphy.gif";
   import Nested from "./lib/Nested.svelte";
 </script>
 
-<h1>Hello {name.toUpperCase()}</h1>
+<h1>Hello {@html name.toUpperCase()}</h1>
 <img {src} alt="{name} alert" />
 <Nested />
 
