a095993eede2a8a186725384e6c41882fa828f28
Aniket Datta <datta.aniket7@gmail.com>
2023-09-21 05:41:25 +0530

    nested-component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script>
   let name = "Stuart";
   import src from "./assets/giphy.gif";
+  import Nested from "./lib/Nested.svelte";
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
 <img {src} alt="{name} alert" />
+<Nested />
 
 <style>
   h1 {
